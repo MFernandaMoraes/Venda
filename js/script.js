310b11b04686507d0ccf6d17341e1751bbf129ac
MFernandaMoraes <ferreira.moraes.maria@escola.pr.gov.br>
2025-06-16 07:58:16 -0300

    Versão 3

--- js/script.js
+++ js/script.js
@@ -4,7 +4,13 @@ alert("Cadastro");
 
 while(contador <=3){
     let nome = prompt("Digite seu nome completo: ");
+    let telefone = prompt("Digite seu telefone: ");
+    let email = prompt("Digite seu email: ");
 
     contador = contador + 1;
-    console.log(nome);
+
+    console.log("Nome completo: " + nome);
+    console.log("Telefone: " + telefone);
+    console.log("Email: " + email);
+
 }
