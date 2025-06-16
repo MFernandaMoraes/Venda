5f3ceed703dc5fc0860d8266b0283f9976b3b750
MFernandaMoraes <ferreira.moraes.maria@escola.pr.gov.br>
2025-06-16 07:47:36 -0300

    Versão 2.1

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ let contador = 0
 alert("Cadastro");
 
 while(contador <=3){
-    let nome = prompt("Digite seu nome completo: ");
+    let(nome) = prompt("Digite seu nome completo: ");
 
     contador = contador + 1;
 }
