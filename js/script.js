60e032137bb45a44eec806da3c7812713fb3d10a
MFernandaMoraes <ferreira.moraes.maria@escola.pr.gov.br>
2025-06-23 11:14:01 -0300

    Versão 4.0

--- js/script.js
+++ js/script.js
@@ -4,21 +4,13 @@ let racao = 100;
 
 while(contador < 3){
 
-    let quantidade_vendiada = prompt("Quantos Kg de ração deseja comprar: ");
+    let quantidade_vendida = (document.getElementById("quantidade").value);
 
-    if(quantidade_vendiada > 100){
+    if(quantidade_vendida > 100){
         alert("Estoque esgotado.");
-    }
-
-    // racao = racao - quantidade_vendiada; 
+    } 
 
     contador = contador + 1;
 
-    console.log(quantidade_vendiada);
-
-    // if(racao = 0){
-    //     alert("Estoque esgotado.");
-    // } else{
-    //     alert("Ainda em estoque.");
-    // }
+    console.log(quantidade_vendida);
 }
