d1c9b55f31d1989c6412ab5747c7932a13bf723d
MFernandaMoraes <ferreira.moraes.maria@escola.pr.gov.br>
2025-06-26 12:21:40 -0300

    Não sei mais a versão

--- js/script.js
+++ js/script.js
@@ -4,16 +4,24 @@ let racao = 100;
 
 function vender(){
 
-while(contador < 1){
+//while(contador < 3){
 
     let quantidade_vendida = document.getElementById("quantidade").value;
 
-    if(quantidade_vendida > 100){
+    if(quantidade_vendida > racao){
         alert("Estoque esgotado.");
-    } 
+    } else {
+        racao = racao - quantidade_vendida;
+    }
 
-    contador = contador + 1;
+    console.log("Ainda em estoque: " + racao);
+    console.log("Quantidade comprada: " + quantidade_vendida);
 
-    alert(quantidade_vendida);
+    console.log(racao);
+    console.log(quantidade_vendida);
+
+     contador = contador + 1;
+
 }
-}
+ 
+//}
